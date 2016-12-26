41709f0b2bffbdba651052bf7783f0e454958436
VivienAdnot <vivien.adnot@gmail.com>
2016-12-26 18:45:24 +0100

    2 first unit test work

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,28 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+
+import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { FriendshipGroupsComponent } from './friendship-groups/friendship-groups.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [AppModule]
+        });
     });
-    TestBed.compileComponents();
-  });
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    it('should have a title', () => {
+        let fixture = TestBed.createComponent(AppComponent);
+        let app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('Friendship');
+    });
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+    it(`should have an element 'app-friendship-groups'`, async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const element = fixture.nativeElement;
+        const friendshipGroupsElement = element.querySelector('app-friendship-groups');
+        expect(friendshipGroupsElement).not.toBeNull('You need a app-friendship-groups component in your root component');
+    }));
 });
